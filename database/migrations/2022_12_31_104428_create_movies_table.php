ca22dc245176a919b556571cf3f973ee843dd453
filipesander <filipesander777@gmail.com>
2023-01-03 20:50:07 -0300

    rename "name" for "title"

--- database/migrations/2022_12_31_104428_create_movies_table.php
+++ database/migrations/2022_12_31_104428_create_movies_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('title');
+            $table->string('description');
             $table->string('category');
             $table->string('duration');
             $table->string('classification');
